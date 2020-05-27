bdc657b8acf2017832f25efc54f3474bb7c5ec62
chirchir <chirchir7370@gmail.com>
2020-05-27 10:58:11 +0300

    updated site title

--- pages/admin.py
+++ pages/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import About
 
 
-admin.site.site_header = "Finetouch Africa"
+admin.site.site_header = "Farm & Greenhouses"
 admin.site.site_title = "Site Admin"
 admin.site.index_title = "Site Admin"
